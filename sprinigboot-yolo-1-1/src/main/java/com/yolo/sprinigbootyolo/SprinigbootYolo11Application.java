3935bb441b8dae840e3539d43698f8f95c89a30b
huanglei <huanglei@mye.hk>
2022-03-21 16:56:54 +0800

    feat: springboot-2.x：快速入门

---
+++ sprinigboot-yolo-1-1/src/main/java/com/yolo/sprinigbootyolo/SprinigbootYolo11Application.java
@@ -0,0 +1,13 @@
+package com.yolo.sprinigbootyolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinigbootYolo11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinigbootYolo11Application.class, args);
+    }
+
+}
