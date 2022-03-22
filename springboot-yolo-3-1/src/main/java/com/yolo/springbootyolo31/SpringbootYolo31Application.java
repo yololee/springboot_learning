76d24e05375b02b5bf479329b9e86f400a8659ea
huanglei <huanglei@mye.hk>
2022-03-22 14:12:50 +0800

    feat: springboot-2.x：RESTful

---
+++ springboot-yolo-3-1/src/main/java/com/yolo/springbootyolo31/SpringbootYolo31Application.java
@@ -0,0 +1,13 @@
+package com.yolo.springbootyolo31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYolo31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootYolo31Application.class, args);
+    }
+
+}
---
+++ springboot-yolo-3-1/src/main/java/com/yolo/springbootyolo31/pojo/User.java
@@ -0,0 +1,16 @@
+package com.yolo.springbootyolo31.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+}
